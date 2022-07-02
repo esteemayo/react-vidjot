77640ccd8ed3e5bf553aafb9e7d2f8e8236f2ff5
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-02 23:23:16 +0100

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,21 @@
+import About from './About';
+import Account from './Account';
+import EditIdea from './EditIdea';
+import Error from './Error';
+import Home from './Home';
+import IdeaForm from './IdeaForm';
+import Ideas from './Ideas';
+import Login from './Login';
+import Register from './Register';
+
+export {
+  About,
+  Account,
+  EditIdea,
+  Error,
+  Home,
+  IdeaForm,
+  Ideas,
+  Login,
+  Register,
+};
