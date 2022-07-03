589ce2ff1f5cb07c92f51820fc3591e48ffe42b8
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-03 20:33:08 +0100

    added AuthProvider

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AppProvider } from './context/GlobalState';
+import { AuthProvider } from 'context/auth/AuthContext';
+import { IdeaProvider } from './context/ideas/IdeaContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <AuthProvider>
+      <IdeaProvider>
+        <App />
+      </IdeaProvider>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
