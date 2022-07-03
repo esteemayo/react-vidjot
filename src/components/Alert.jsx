0d2eb1094a7d3ee93431e678d73b818ae834718c
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-03 20:49:52 +0100

    refactored

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,16 +1,20 @@
 import { useEffect } from 'react';
-import { useGlobalContext } from '../context/ideas/IdeaContext';
+
+import { useGlobalContext } from 'context/ideas/IdeaContext';
+import { useGlobalAuthContext } from 'context/auth/AuthContext';
 
 const Alert = ({ msg, type }) => {
   const { ideas, hideAlert } = useGlobalContext();
+  const { hideModal } = useGlobalAuthContext();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       hideAlert();
+      hideModal();
     }, 3000);
 
     return () => clearTimeout(timeout);
-  }, [ideas, hideAlert]);
+  }, [ideas, hideAlert, hideModal]);
 
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
