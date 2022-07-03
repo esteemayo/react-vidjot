6843903dbbb6501a15fc4f85a48d47fd24bc6f57
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-03 21:03:34 +0100

    renamed files and changed directory

---
+++ src/context/ideas/IdeaContext.js
@@ -0,0 +1,60 @@
+import { toast } from 'react-toastify';
+import { useReducer, useContext, createContext } from 'react';
+
+import * as actions from './IdeaTypes';
+import IdeaReducer from './IdeaReducer';
+import { deleteIdea } from 'services/ideaService';
+
+const INITIAL_STATE = {
+  ideas: [],
+  loading: true,
+  alert: {
+    show: false,
+    type: '',
+    msg: '',
+  },
+};
+
+const IdeaContext = createContext(INITIAL_STATE);
+
+const IdeaProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(IdeaReducer, INITIAL_STATE);
+
+  const addIdea = (idea) => {
+    dispatch({ type: actions.ADD_IDEA, payload: idea });
+  };
+
+  const handleDelete = async (id) => {
+    const originalIdeas = [...state.ideas];
+    dispatch({
+      type: actions.DELETE_IDEA,
+      payload: id,
+    });
+
+    try {
+      await deleteIdea(id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        toast.error('This idea has already been deleted!');
+      INITIAL_STATE.ideas = originalIdeas;
+    }
+  };
+
+  const hideAlert = () => {
+    dispatch({ type: actions.REMOVE_ALERT });
+  };
+
+  return (
+    <IdeaContext.Provider
+      value={{ ...state, dispatch, addIdea, hideAlert, handleDelete }}
+    >
+      {children}
+    </IdeaContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(IdeaContext);
+};
+
+export { IdeaProvider };
---
+++ src/context/ideas/IdeaReducer.js
@@ -0,0 +1,54 @@
+import * as actions from './IdeaTypes';
+
+const IdeaReducer = (state, { payload, type }) => {
+  if (type === actions.DISPLAY_IDEAS) {
+    return {
+      ...state,
+      ideas: payload,
+    };
+  }
+
+  if (type === actions.LOADING) {
+    return {
+      ...state,
+      loading: false,
+    };
+  }
+
+  if (type === actions.ADD_IDEA) {
+    return {
+      ...state,
+      ideas: [payload, ...state.ideas],
+      alert: {
+        show: true,
+        type: 'success',
+        msg: 'Video idea added successfully.',
+      },
+    };
+  }
+
+  if (type === actions.DELETE_IDEA) {
+    return {
+      ...state,
+      ideas: state.ideas.filter((idea) => idea.id !== payload),
+      alert: {
+        show: true,
+        type: 'danger',
+        msg: 'Video idea deleted successfully.',
+      },
+    };
+  }
+
+  if (type === actions.REMOVE_ALERT) {
+    return {
+      ...state,
+      alert: {
+        show: false,
+      },
+    };
+  }
+
+  throw new Error(`No matching action type → ${type}`);
+};
+
+export default IdeaReducer;
