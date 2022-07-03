69de1426f2a81db05a72d89ad21bfbf566f19747
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-03 21:00:00 +0100

    first commit

---
+++ src/context/ideas/IdeaTypes.js
@@ -0,0 +1,5 @@
+export const DISPLAY_IDEAS = 'DISPLAY_IDEAS';
+export const LOADING = 'LOADING';
+export const ADD_IDEA = 'ADD_IDEA';
+export const DELETE_IDEA = 'DELETE_IDEA';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
