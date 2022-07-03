8a3536d30cbb7b5a1526d4437e97d5d983cce3e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-03 23:25:00 +0100

    refactored

--- src/services/ideaService.js
+++ src/services/ideaService.js
@@ -2,26 +2,14 @@ import http from './httpService';
 
 const apiEndPoint = '/ideas';
 
-function ideaUrl(id) {
-  return `${apiEndPoint}/${id}`;
-}
+const ideaUrl = (id) => `${apiEndPoint}/${id}`;
 
-export function getIdeas() {
-  return http.get(apiEndPoint);
-}
+export const getIdeas = () => http.get(apiEndPoint);
 
-export function getIdea(id) {
-  return http.get(ideaUrl(id));
-}
+export const getIdea = (id) => http.get(ideaUrl(id));
 
-export function createIdea(idea) {
-  return http.post(apiEndPoint, idea);
-}
+export const createIdea = (idea) => http.post(apiEndPoint, idea);
 
-export function updateIdea(id, idea) {
-  return http.patch(ideaUrl(id), idea);
-}
+export const updateIdea = (id, idea) => http.patch(ideaUrl(id), idea);
 
-export function deleteIdea(id) {
-  return http.delete(ideaUrl(id));
-}
+export const deleteIdea = (id) => http.delete(ideaUrl(id));
