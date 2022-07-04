c5d2f312656d85d6752ab0a3cf56c5cbabe2e3c1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-04 01:50:26 +0100

    modified

--- src/formData.js
+++ src/formData.js
@@ -28,3 +28,48 @@ export const registerInputs = [
     placeholder: '********',
   },
 ];
+
+export const userDataInputs = [
+  {
+    id: 'name',
+    name: 'name',
+    type: 'name',
+    label: 'Name',
+  },
+  {
+    id: 'email',
+    name: 'email',
+    type: 'email',
+    label: 'Email',
+  },
+  {
+    id: 'username',
+    name: 'username',
+    type: 'text',
+    label: 'Username',
+  },
+];
+
+export const updatePasswordInputs = [
+  {
+    id: 'passwordCurrent',
+    name: 'passwordCurrent',
+    type: 'password',
+    label: 'Current Password',
+    placeholder: '********',
+  },
+  {
+    id: 'password',
+    name: 'password',
+    type: 'password',
+    label: 'Password',
+    placeholder: '********',
+  },
+  {
+    id: 'passwordConfirm',
+    name: 'passwordConfirm',
+    type: 'password',
+    label: 'Confirm Password',
+    placeholder: '********',
+  },
+];
