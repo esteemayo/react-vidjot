6de83d3bc2a1d7057b89aaf3cf2ee17f796fb584
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-17 13:49:34 +0100

    added new action types

--- src/context/auth/AuthTypes.js
+++ src/context/auth/AuthTypes.js
@@ -1,3 +1,5 @@
-export const LOGIN = 'LOGIN';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGOUT = 'LOGOUT';
 export const REMOVE_MODAL = 'REMOVE_MODAL';
