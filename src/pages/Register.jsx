21355e6f89c240f0f1d1439914ddeef6bde54ec0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-02 13:38:15 +0100

    feat: handle user' photo upload

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -83,6 +83,42 @@ const Register = () => {
   const handleRegister = async () => {
     loginStart();
     try {
+      if (file) {
+        const fileName = `${new Date().getTime()}-${file.name}`;
+
+        const storage = getStorage(app);
+        const storageRef = ref(storage, `images/${fileName}`);
+
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+
+              default:
+                break;
+            }
+          },
+          (error) => {
+            console.log(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              console.log('File available at', downloadURL);
+              setValues((prev) => ({ ...prev, photo: downloadURL }));
+            });
+          }
+        );
+      }
+
       const { data } = await register({ ...values });
       loginSuccess(data);
     } catch (ex) {
