d1bc68cc203514033dfad362f8058ca5afd5650c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-02 16:19:55 +0100

    fix: fix image upload error

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -76,49 +76,18 @@ const Register = () => {
     if (!validateForm()) return;
     setErrors({});
 
-    await handleRegister();
+    if (!file) {
+      await handleRegister();
+    } else {
+      await handleUpload();
+    }
+
     user && await window.location.replace('/ideas');
   };
 
   const handleRegister = async () => {
     loginStart();
     try {
-      if (file) {
-        const fileName = `${new Date().getTime()}-${file.name}`;
-
-        const storage = getStorage(app);
-        const storageRef = ref(storage, `images/${fileName}`);
-
-        const uploadTask = uploadBytesResumable(storageRef, file);
-
-        uploadTask.on('state_changed',
-          (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-              case 'paused':
-                console.log('Upload is paused');
-                break;
-              case 'running':
-                console.log('Upload is running');
-                break;
-
-              default:
-                break;
-            }
-          },
-          (error) => {
-            console.log(error);
-          },
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              console.log('File available at', downloadURL);
-              setValues((prev) => ({ ...prev, photo: downloadURL }));
-            });
-          }
-        );
-      }
-
       const { data } = await register({ ...values });
       loginSuccess(data);
     } catch (ex) {
@@ -133,6 +102,48 @@ const Register = () => {
       }
       loginFailure(ex);
     }
+  };
+
+  const handleUpload = async () => {
+    const fileName = `${new Date().getTime()}-${file.name}`;
+
+    const storage = getStorage(app);
+    const storageRef = ref(storage, `images/${fileName}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+
+          default:
+            break;
+        }
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          console.log('File available at', downloadURL);
+          const newUser = {
+            ...values,
+            photo: downloadURL,
+          };
+
+          const { data } = await register({ ...newUser });
+          loginSuccess(data);
+        });
+      }
+    );
   };
 
   useEffect(() => {
